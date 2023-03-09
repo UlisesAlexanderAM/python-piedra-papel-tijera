2ceee205fd87b23a05d544fd145e54af9b8df206
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2023-03-09 16:16:07 -0600

    refactor: move functionality to main function

--- main.py
+++ main.py
@@ -15,94 +15,99 @@ Cualquier otro numero - Salir del juego
 
 INPUT_MSG = "Teclee el numero de la opción deseada: "
 
-print(INSTRUCTIONS)
 
-while True:
-    computer_wins = 0
-    user_wins = 0
-    rounds = 1
-    computer_score = f"Victorias de la computadora: {computer_wins}"
-    user_score = f"Victorias del usuario: {user_wins}"
+def main():
+    print(INSTRUCTIONS)
+    while True:
+        computer_wins = 0
+        user_wins = 0
+        rounds = 1
+        computer_score = f"Victorias de la computadora: {computer_wins}"
+        user_score = f"Victorias del usuario: {user_wins}"
 
-    user_option = input(INPUT_MSG)
-    LEN_INPUT_MSG = len(INPUT_MSG)
-    ASTERISKS = "*" * LEN_INPUT_MSG
+        user_option = input(INPUT_MSG)
+        LEN_INPUT_MSG = len(INPUT_MSG)
+        ASTERISKS = "*" * LEN_INPUT_MSG
 
-    score = "\n".join(["Marcador", computer_score, user_score, ASTERISKS])
+        score = "\n".join(["Marcador", computer_score, user_score, ASTERISKS])
 
-    if not user_option.isdigit():
-        print("Opción invalida")
-        continue
+        if not user_option.isdigit():
+            print("Opción invalida")
+            continue
 
-    if user_option == "4":
+        if user_option == "4":
+            print(score)
+            continue
+
+        if user_option not in LIST_OF_OPTIONS:
+            print("Gracias por jugar.¡Adios!")
+            break
+
+        print(ASTERISKS)
+        print(f"RONDA {rounds}")
+        print(ASTERISKS)
+
         print(score)
-        continue
 
-    if user_option not in LIST_OF_OPTIONS:
-        print("Gracias por jugar.¡Adios!")
-        break
+        computer_option = random.choice(LIST_OF_OPTIONS)
 
-    print(ASTERISKS)
-    print(f"RONDA {rounds}")
-    print(ASTERISKS)
+        user_opt_str = OPTIONS[user_option]
+        comp_opt_str = OPTIONS[computer_option]
+        print(f"El usuario escogio: {user_opt_str}")
+        print(f"La computadora escogio: {comp_opt_str}")
 
-    print(score)
+        print(ASTERISKS)
 
-    computer_option = random.choice(LIST_OF_OPTIONS)
+        if user_option == computer_option:
+            print("Empate")
+            print(score)
+            rounds += 1
+            continue
 
-    user_opt_str = OPTIONS[user_option]
-    comp_opt_str = OPTIONS[computer_option]
-    print(f"El usuario escogio: {user_opt_str}")
-    print(f"La computadora escogio: {comp_opt_str}")
-
-    print(ASTERISKS)
-
-    if user_option == computer_option:
-        print("Empate")
-        print(score)
-        rounds += 1
-        continue
-
-    user_win_str = f"Gana usuario: {user_opt_str} le gana a {comp_opt_str}".join(
-        ["\n", ASTERISKS]
-    )
-    computer_win_str = (
-        f"Gana computadora: {comp_opt_str} le gana a {user_opt_str}".join(
+        user_win_str = f"Gana usuario: {user_opt_str} le gana a {comp_opt_str}".join(
             ["\n", ASTERISKS]
         )
-    )
+        computer_win_str = (
+            f"Gana computadora: {comp_opt_str} le gana a {user_opt_str}".join(
+                ["\n", ASTERISKS]
+            )
+        )
 
-    if user_option == "1" and computer_option == "2":
-        computer_wins += 1
-        rounds += 1
-        print(computer_win_str)
-    elif user_option == "1" and computer_option == "3":
-        user_wins += 1
-        rounds += 1
-        print(user_win_str)
+        if user_option == "1" and computer_option == "2":
+            computer_wins += 1
+            rounds += 1
+            print(computer_win_str)
+        elif user_option == "1" and computer_option == "3":
+            user_wins += 1
+            rounds += 1
+            print(user_win_str)
 
-    if user_option == "2" and computer_option == "1":
-        user_wins += 1
-        rounds += 1
-        print(user_win_str)
-    elif user_option == "2" and computer_option == "3":
-        computer_wins += 1
-        rounds += 1
-        print(computer_win_str)
+        if user_option == "2" and computer_option == "1":
+            user_wins += 1
+            rounds += 1
+            print(user_win_str)
+        elif user_option == "2" and computer_option == "3":
+            computer_wins += 1
+            rounds += 1
+            print(computer_win_str)
 
-    if user_option == "3" and computer_option == "1":
-        computer_wins += 1
-        rounds += 1
-        print(computer_win_str)
-    elif user_option == "3" and computer_option == "2":
-        user_wins += 1
-        rounds += 1
-        print(user_win_str)
+        if user_option == "3" and computer_option == "1":
+            computer_wins += 1
+            rounds += 1
+            print(computer_win_str)
+        elif user_option == "3" and computer_option == "2":
+            user_wins += 1
+            rounds += 1
+            print(user_win_str)
 
-    if computer_wins == 2:
-        print("El ganador es la computadora")
-        break
+        if computer_wins == 2:
+            print("El ganador es la computadora")
+            break
 
-    if user_wins == 2:
-        print("El ganador es el usuario")
-        break
+        if user_wins == 2:
+            print("El ganador es el usuario")
+            break
+
+
+if __name__ == "__main__":
+    main()
